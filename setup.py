fafe7c299988da84c705a6aa00e22cea157c78c7
zx013 <1453145267@qq.com>
2019-11-24 11:56:20 +0800

    clean参数

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+import os
+import shutil
+from setuptools import setup, find_packages, Command
+
+class Clean(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def remove(self, remove_list):
+        for name in remove_list:
+            if not os.path.exists(name):
+                continue
+            shutil.rmtree(name)
+
+    def run(self):
+        self.remove(['build', 'dist', 'pyvoxel.egg-info'])
 
 setup(
     name = 'pyvoxel',
@@ -8,6 +27,12 @@ setup(
     author = 'zx013',
     url = 'https://github.com/zx013/pyvoxel',
     license = 'MIT',
+    keywords = ['voxel'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
     packages = find_packages(),
     python_requires = '>=3.5',
     setup_requires = [
@@ -15,5 +40,9 @@ setup(
     ],
     install_requires = [
         #'panda3d>=1.10.4.1'
-    ]
+    ],
+    cmdclass = {
+        'clean': Clean,
+    },
+    zip_safe = False
 )
