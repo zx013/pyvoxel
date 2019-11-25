5e08ab694f7883f3385284f6b9a0db48ea8e0d61
zx013 <1453145267@qq.com>
2019-11-26 00:36:12 +0800

    类似kv的解析方式

--- pyvoxel/config.py
+++ pyvoxel/config.py
@@ -61,6 +61,48 @@ class Config(object):
         self.plugins = Manager.get_plugins() #已有的插件类
         self.load('config/testconfig.vx')
 
+    #分割后去空格
+    def _strip_split(self, info, sep, maxsplit=-1):
+        return [s.strip() for s in info.split(sep, maxsplit)]
+
+    #类名称是否合法
+    def _legal_class(self, class_name):
+        if not class_name: #名称为空
+            return False
+        if class_name[0] not in self.LEGAL_CLASS: #首字母不是大写
+            return False
+        for s in class_name:
+            if s not in self.LEGAL_VAR:
+                return ''
+        return True
+
+    def _check_class(self, class_name, plugins):
+        #类名称不合法
+        if not self._legal_class(class_name):
+            return False
+
+        #类不存在
+        if class_name not in self.plugins and class_name not in plugins:
+            return False
+        return True
+
+    #判断变量是否合法
+    def _legal_var(self, var_name):
+        if not var_name: #名称为空
+            return False
+        for s in var_name:
+            if s not in self.LEGAL_VAR:
+                return ''
+        return True
+
+    def analyse(self, expr):
+        '''
+        解析表达式
+        '''
+        #root，根节点
+        #self，自身节点
+        pass
+
     def load(self, name):
         with open(name, 'r') as fp:
             lines = fp.readlines()
@@ -108,14 +150,21 @@ class Config(object):
             if not key: #键值为空
                 print('Key is empty')
                 return False
-            if key[0] == '<' or key[-1] == '>':
+
+            #类的定义
+            if space == 0:
                 if key[0] != '<' or key[-1] != '>': #键值格式不对
                     print('Key is not right')
                     return False
+                
+                if val:
+                    print('Val is not empty')
+                    return False
+
                 #类声明
                 key = key[1:-1].strip()
                 if not key: #键值为空
-                    print('Key is empty')
+                    print('Class is empty')
                     return False
 
                 split_key = self._strip_split(key, '@')
@@ -124,47 +173,54 @@ class Config(object):
                     return False
 
                 class_name = split_key[0]
-                if not self._legal_class(class_name): #类命不合法
-                    print('Class is illegal', class_name)
-                    return False
 
-                if len(split_key) == 2: #新建类并设定格式
-                    if space != 0: #新建类不能在其他类中定义
-                        print('New class can not be define in other class')
+                if len(split_key) == 2:
+                    if not self._legal_class(class_name): #类命不合法
+                        print('Class is illegal', class_name)
                         return False
-                    base_class = class_name
 
+                    base_class = class_name
+    
                     base_list = self._strip_split(split_key[1], '+')
                     for base in base_list:
-                        if not self._legal_class(base):
-                            print('Class is illegal', base)
+                        if not self._check_class(base, plugins): #类命不合法
+                            print('Class is illegal or not exist', class_name)
                             return False
-                        if base not in self.plugins and base not in plugins:
-                            print('Class not exist', base)
-                            return False
+
                     plugins.add(class_name)
 
+                    attr_space = space + 1
                     process_data.append(('nclass', line_number, space, class_name, base_list))
-                else: #设定已有类格式
-                    if space == 0:
-                        base_class = ''
-                    if class_name not in self.plugins and class_name not in plugins:
-                        print('Class not exist', class_name)
+                else:
+                    if not self._check_class(class_name, plugins): #类命不合法
+                        print('Class is illegal or not exist', class_name)
                         return False
+
+                    base_class = ''
+
+                    attr_space = space + 1
+                    process_data.append(('class', line_number, space, class_name))
+            else:
+                if not val: #设定已有类格式
+                    class_name = key
+                    if not self._check_class(class_name, plugins): #类命不合法
+                        print('Class is illegal or not exist', class_name)
+                        return False
+
                     if class_name == base_class: #新建类不能作为该类的子节点
                         print('New class can not be subclass')
                         return False
+                    attr_space = space + 1
                     process_data.append(('class', line_number, space, class_name))
-                attr_space = space + 1
-            else:
-                if space != attr_space: #属性值必须跟在类定义后面
-                    print('Attribute must follow class')
-                    return False
-                if not self._legal_var(key):
-                    print('Var is illegal')
-                    return False
+                else:
+                    if space != attr_space: #属性值必须跟在类定义后面
+                        print('Attribute must follow class')
+                        return False
+                    if not self._legal_var(key):
+                        print('Var is illegal')
+                        return False
 
-                process_data.append(('data', line_number, space, key, val))
+                    process_data.append(('data', line_number, space, key, val))
 
             last_space = space
 
@@ -175,6 +231,7 @@ class Config(object):
             line = line[3:]
             if line_type == 'data':
                 key, val = line[:2]
+                self.analyse(val)
                 cursor_node.add((key, val))
             else:
                 class_name = line[0]
@@ -190,30 +247,6 @@ class Config(object):
 
                 cursor_node = node
         self.root.show()
-
-    #分割后去空格
-    def _strip_split(self, info, sep, maxsplit=-1):
-        return [s.strip() for s in info.split(sep, maxsplit)]
-
-    #类名称是否合法
-    def _legal_class(self, class_name):
-        if not class_name: #名称为空
-            return False
-        if class_name[0] not in self.LEGAL_CLASS: #首字母不是大写
-            return False
-        for s in class_name:
-            if s not in self.LEGAL_VAR:
-                return ''
-        return True
-
-    #判断变量是否合法
-    def _legal_var(self, var_name):
-        if not var_name: #名称为空
-            return False
-        for s in var_name:
-            if s not in self.LEGAL_VAR:
-                return ''
-        return True
 
 
 if __name__ == '__main__':
