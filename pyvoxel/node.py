c8b4312adaef6c159e2b654710462b16808d7d87
unknown <zhuziyao_sx@qiyi.com>
2019-12-02 16:10:00 +0800

    通过bind绑定后同步不同类的数据

---
+++ pyvoxel/node.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import re
+from pyvoxel.log import Log
+
+
+#类中attr属性改变时触发on_attr事件，同时同步改变关联的值
+class Node(object):
+    def __init__(self):
+        self._trigger = {}
+        self._reflex = {}
+        self.parent = None
+        self.children = []
+
+    def __setattr__(self, name, value):
+        ovalue = self.__dict__.get(name, None)
+        self.__dict__[name] = value
+
+        try:
+            self._on_func(name, ovalue, value)
+        except Exception as ex:
+            Log.error(ex)
+
+    #调用on_函数
+    def _on_func(self, name, ovalue, value):
+        on_name = 'on_' + name
+        if on_name in self.__dict__:
+            on_func = self.__dict__[on_name]
+            on_func(ovalue, value)
+
+        if name in self._trigger:
+            for node, nname in self._trigger[name]:
+                node._update_value(nname, (self, name), value)
+
+    #更新关联类的值
+    def _update_value(self, name, base, basev):
+        try:
+            expr, pattern, local = self._reflex[name]
+            local[pattern[base]] = basev
+
+            value = eval(expr, None, local)
+            setattr(self, name, value)
+        except Exception as ex:
+            Log.error('Sync class data error : ' + str(ex))
+
+    def add_node(self, node):
+        self.children.append(node)
+        if node.parent:
+            Log.warning('{node} already has parent'.format(node=node))
+        node.parent = self
+
+    #base_cls中的base_name字段改变时，同步修改self的name字段
+    def bind(self, name, expr):
+        if '__import__' in expr: #literal_eval不能设置locals，因此需要对expr进行判断
+            print('Expr can not use __import__.')
+            return False
+
+        pattern = {}
+        pset = set()
+        for i, p in enumerate(re.finditer(u'(?:(?:p|c[0-9]+).)+[a-z0-9_]+', expr, flags=re.I)):
+            iname = 'x{i}'.format(i=i)
+            pname = p.group()
+            if pname in pset: #防止重复定义
+                continue
+
+            base_cls = self
+            plist = pname.split('.')
+            try:
+                for node in plist[:-1]:
+                    if node[0] in ('P', 'p'):
+                        base_cls = base_cls.parent
+                    elif node[0] in ('C', 'c'):
+                        base_cls = base_cls.children[int(node[1:])]
+            except Exception as ex:
+                print('Expr error, please check it : ', ex)
+                return False
+            base_name = plist[-1]
+
+            pset.add(pname)
+            pattern[(base_cls, base_name)] = iname
+            expr = expr.replace(pname, iname)
+
+        try:
+            local = {}
+            for base, key in pattern.items():
+                base_cls, base_name = base
+                local[key] = getattr(base_cls, base_name)
+            value = eval(expr, None, local)
+            setattr(self, name, value)
+        except Exception as ex:
+            print('Expr error, please check it : ', ex)
+            return False
+        
+        #能够正常获取参数值时，写入配置变量
+        for base_cls, base_name in pattern.keys():
+            base_cls._trigger.setdefault(base_name, set())
+            base_cls._trigger[base_name].add((self, name))
+        self._reflex[name] = (expr, pattern, local)
+        return True
+
+
+if __name__ == '__main__':
+    t1 = Node()
+    t2 = Node()
+    t3 = Node()
+    t4 = Node()
+
+    t1.add_node(t2)
+    t2.add_node(t3)
+    t3.add_node(t4)
+
+    t1.testa = 1
+    t1.testb = 2
+    t3.testc = 3
+
+    t2.bind('testd', 'p.testa + p.testb * p.testb - c0.testc')
+    t4.bind('teste', 'p.p.testd + p.p.p.testa')
+    print(t2.testd, t4.teste)
+
+    t1.testa = 4
+    print(t2.testd, t4.teste)
+
+    t1.testb = 5
+    print(t2.testd, t4.teste)
+
+    t3.testc = 6
+    print(t2.testd, t4.teste)
